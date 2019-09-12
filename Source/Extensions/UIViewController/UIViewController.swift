cabf220f8fef445457126bda3b9907b606b07062
Alexandr Honcharenko <alex.devios93@gmail.com>
2019-09-12 11:32:52 +0300

    - add custom class

--- Source/Extensions/UIViewController/UIViewController.swift
+++ Source/Extensions/UIViewController/UIViewController.swift
@@ -2,7 +2,24 @@ import UIKit
 import Foundation
 
 extension UIViewController {
-    func getNameOfCreator() -> String {
+    public func getNameOfCreator() -> String {
         return "Alex"
+    }
+}
+
+class CustomTextView: UITextView {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //
+        self.backgroundColor = .red
+    }
+    override init(frame: CGRect, textContainer: NSTextContainer?) {
+        super.init(frame: frame, textContainer: textContainer)
+        //
+        self.backgroundColor = .red
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
 }
