35080632411e141a83649538e4bbc1474ccb23a8
Alexandr Honcharenko <alex.devios93@gmail.com>
2019-09-12 10:25:18 +0300

    Initial Commit

---
+++ FirstAidPod/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstAidPod
+//
+//  Created by Alexandr Honcharenko on 9/12/19.
+//  Copyright © 2019 Alexandr Honcharenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
